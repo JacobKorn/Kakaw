2c9110f77863b971f3c4779fe83ce31c57d91e0e
georgefeast <georgefeast@gmail.com>
2014-11-13 21:21:27 +1300

    View outline

---
+++ db/seeds.rb
@@ -0,0 +1,27 @@
+require 'faker'
+
+users = []
+
+10.times do |user|
+  user = User.create(username: Faker::Internet.user_name, email: Faker::Internet.email, password: Faker::Internet.password, avatar: Faker::Avatar.image("kakaw", "200x200", "jpg"))
+  users << user
+end
+
+puts users
+
+song_urls = ["https://soundcloud.com/1080pcollection/04-idgafam", "https://soundcloud.com/1080pcollection/mongo-skato-turismoc-1",
+            "https://soundcloud.com/dimension_uk/movefaster", "http://youtu.be/B9rSBcoX9ak", "http://youtu.be/0TIIu9CERgI",
+            "http://youtu.be/RubBzkZzpUA"]
+
+songs = []
+
+20.times do |song|
+  song = Song.create(artist: Faker::App.name, title: Faker::Commerce.product_name, link: song_urls.sample)
+  songs << song
+end
+
+puts songs
+
+40.times do |share|
+  share = Share.create(user_id: users.sample.id, song_id: songs.sample.id)
+end
