e749e8b3dc3fe4f11dd682c7769a930466a7cbde
Steve Liew <ys.steve@gmail.com>
2014-11-13 18:33:27 +1300

    URL Validator done

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,17 @@
+require 'uri'
+
 class Song < ActiveRecord::Base
-  # Remember to create a migration!
   has_many :shares
   has_many :users, :through => :shares
+
+  def self.url_valid?(url)
+    if url =~ /\A#{URI::regexp(['http', 'https'])}\z/
+      if url =~ /soundcloud/
+        true
+    else
+      false
+    end
+    end
+  end
+
 end
