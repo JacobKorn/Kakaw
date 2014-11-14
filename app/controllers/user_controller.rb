f99f1f2dc32d075711088cb0149dff7a5dd40f68
georgefeast <georgefeast@gmail.com>
2014-11-14 16:35:41 +1300

    Finished argh

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,6 @@
-get '/users/:id' do
+get '/users/profile/:id' do
   @user = User.find(params[:id])
+  @shares = Share.where(user_id: @user.id).reverse
   @gravatar = current_user.gravatar if current_user
   erb :profile
 end
